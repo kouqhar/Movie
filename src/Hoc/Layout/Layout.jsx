24be0b2c77122a1a00ad709c82144c65f857ba42
kouqhar <naphtaliduniya2@gmail.com>
2024-11-03 15:45:26 +0100

    Scroll to view error2

--- src/Hoc/Layout/Layout.jsx
+++ src/Hoc/Layout/Layout.jsx
@@ -1,20 +1,22 @@
-import { useEffect, useRef } from "react";
-import { Outlet, useLocation } from "react-router-dom";
+// import { useEffect, useRef } from "react";
+// import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 //  styles
 import styles from "./styles/styles.module.css";
 
 const Layout = () => {
-  const toTopRef = useRef(null);
-  const location = useLocation();
+  // const toTopRef = useRef(null);
+  // ref={toTopRef}
+  // const location = useLocation();
 
-  useEffect(() => {
-    toTopRef.current.scrollIntoView({ behavior: "smooth" });
-  }, [location]);
+  // useEffect(() => {
+  //   toTopRef.current.scrollIntoView({ behavior: "smooth" });
+  // }, [location]);
 
   return (
     <div>
-      <main ref={toTopRef} className={styles.container}>
+      <main className={styles.container}>
         <Outlet />
       </main>
     </div>
